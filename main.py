2546c9644ef2c652f2517c999ed1d4b45cbf20ed
Arthur Pedro <arthurpedro.profissional@gmail.com>
2025-04-16 20:23:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ URL_CARDS = 'https://api.clashroyale.com/v1/cards'
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE1ZDU1MzY4LTRhMDMtNGMyMi05Yzk0LTBmMjM4MmUzNWRlNSIsImlhdCI6MTc0NDc2MTcyNCwic3ViIjoiZGV2ZWxvcGVyLzJhNzRiOWNhLTUzMTEtNzZkNi01ZDlkLWUxZTZhOTdkY2I5NSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNDMuMjA4LjEyOC4xMjIiXSwidHlwZSI6ImNsaWVudCJ9XX0.9xkwxR25EGBq2qcMopXhYa4U7JDg61sBWJ5jtN-M3InkHz5cuXXRDjEFQXGJk24e4CCEgjfVFV2YeAuh1bfHAg'
+    'Authorization': 'Bearer'
 }
 
 def get_all_cards():
